f7237c5f2d22bb853eb40aca5ba23fb48d5a2505
AminForouzan <amin.pqr@gmail.com>
2025-07-21 20:23:06 +0330

    Improve and change class and id names

--- script.js
+++ script.js
@@ -3,53 +3,52 @@ const isDesktop = window.matchMedia("(min-width: 64em)");
 
 const authorSection = document.getElementById("author-section");
 
-const profileBlock = document.getElementById("profileBlock");
-const profileShareBtn = document.getElementById("profileShareBtn");
-const profileShareBtnWrapper = profileBlock.querySelector(
-  ".profile-share-btn-wrapper"
+const authorProfile = document.getElementById("author-profile");
+const authorProfileBtn = document.getElementById("author-profile-btn");
+const profileShareBtnContainer = authorProfile.querySelector(
+  ".share-btn-container"
 );
 
-const socialBlock = document.getElementById("socialBlock");
-const socialShareBtn = document.getElementById("socialShareBtn");
+const socialSharePanel = document.getElementById("social-share-panel");
+const socialPanelBtn = document.getElementById("social-panel-btn");
 
 function handleShareBtnMobile() {
-  profileBlock.classList.toggle("hide");
-  socialBlock.classList.toggle("hide");
+  authorProfile.classList.toggle("hide");
+  socialSharePanel.classList.toggle("hide");
 }
 
 function handleShareBtnDesktop() {
-  socialBlock.classList.toggle("hide");
+  socialSharePanel.classList.toggle("hide");
 }
 
 // Initial event listeners
 if (isDesktop.matches) {
-  profileShareBtnWrapper.appendChild(socialBlock);
-  profileShareBtn.addEventListener("click", handleShareBtnDesktop);
+  profileShareBtnContainer.appendChild(socialSharePanel);
+  authorProfileBtn.addEventListener("click", handleShareBtnDesktop);
 } else {
-  profileShareBtn.addEventListener("click", handleShareBtnMobile);
-  socialShareBtn.addEventListener("click", handleShareBtnMobile);
+  authorProfileBtn.addEventListener("click", handleShareBtnMobile);
+  socialPanelBtn.addEventListener("click", handleShareBtnMobile);
 }
 
 // Screen width Resizes
 isDesktop.addEventListener("change", (e) => {
   if (e.matches) {
-    console.log("Mobile --> desktop");
     // Mobile --> Desktop
     // first, remove event listeners that were for mobile
-    profileShareBtn.removeEventListener("click", handleShareBtnMobile);
-    socialShareBtn.removeEventListener("click", handleShareBtnMobile);
-    // then, relocate the socialBlock element
-    profileShareBtnWrapper.appendChild(socialBlock);
-    // finally, add the click event to profileBlockBtn
-    profileShareBtn.addEventListener("click", handleShareBtnDesktop);
+    authorProfileBtn.removeEventListener("click", handleShareBtnMobile);
+    socialPanelBtn.removeEventListener("click", handleShareBtnMobile);
+    // then, relocate the socialSharePanel element
+    profileShareBtnContainer.appendChild(socialSharePanel);
+    // finally, add the click event to authorProfileBtn
+    authorProfileBtn.addEventListener("click", handleShareBtnDesktop);
   } else {
     // Desktop --> Mobile
     // first, remove event listener that were for desktop
-    profileShareBtn.removeEventListener("click", handleShareBtnDesktop);
-    // then, relocate the socialBlock element
-    authorSection.appendChild(socialBlock);
+    authorProfileBtn.removeEventListener("click", handleShareBtnDesktop);
+    // then, relocate the socialSharePanel element
+    authorSection.appendChild(socialSharePanel);
     // finally, add mobile event listeners
-    profileShareBtn.addEventListener("click", handleShareBtnMobile);
-    socialShareBtn.addEventListener("click", handleShareBtnMobile);
+    authorProfileBtn.addEventListener("click", handleShareBtnMobile);
+    socialPanelBtn.addEventListener("click", handleShareBtnMobile);
   }
 });
